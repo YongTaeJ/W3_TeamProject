67f5a50593e54b63d804f55f91e63b4e9abf289d
YongTaeJ <g_o_robot@naver.com>
2023-11-15 16:05:18 +0900

    기반 Scene Class 생성
    작업 편의성을 위해 BaseScene과 SceneManager 클래스를 추가합니다.

---
+++ W3_TeamProject/Scenes/BaseScripts/BaseScene.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaDungeon
+{
+	internal abstract class BaseScene
+	{
+		public State beforeState = State.None;
+		protected State nextState = State.None;
+		public abstract void EnterScene();
+		public abstract State ExitScene();
+		public void ClearKey()
+		{
+			nextState = State.None;
+		}
+	}
+}
---
+++ W3_TeamProject/Scenes/BaseScripts/SceneManager.cs
@@ -0,0 +1,54 @@
+﻿using SpartaDungeon.Scenes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SpartaDungeon
+{
+	enum State
+	{
+		None,
+		End,
+		Main,
+		Town,
+		Status,
+		Inventory,
+		Shop,
+		Dungeon
+	}
+
+	internal class SceneManager
+	{
+		Dictionary<State, BaseScene> Scenes = new Dictionary<State, BaseScene>();
+		State currentState, beforeState;
+
+		public void ProcessScene(State state)
+		{
+			Scenes[state].ClearKey();
+			Scenes[state].beforeState = beforeState;
+			Scenes[state].EnterScene();
+			beforeState = state;
+			currentState = Scenes[state].ExitScene();
+		}
+
+		public void Init()
+		{
+			// Scenes.Add(State.End, new EndScene() ); 추후 구현 필요!!
+			Scenes.Add(State.Main, new MainScene() );
+			Scenes.Add(State.Town, new TownScene() );
+			Scenes.Add(State.Status, new StatusScene() );
+			Scenes.Add(State.Inventory, new InventoryScene());
+			Scenes.Add(State.Shop, new ShopScene() );
+			Scenes.Add(State.Dungeon, new DungeonScene());
+		}
+
+		public State Update()
+		{
+			return currentState;
+		}
+	}
+}
