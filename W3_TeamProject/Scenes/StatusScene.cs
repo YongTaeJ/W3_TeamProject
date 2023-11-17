dca49c5c9d260a382606290f7f512bdc68dfb6d9
Lawrence1031h <lawrence1031h@gmail.com>
2023-11-17 15:28:13 +0900

    20231117_중간저장
    15:28

--- W3_TeamProject/Scenes/StatusScene.cs
+++ W3_TeamProject/Scenes/StatusScene.cs
@@ -24,8 +24,8 @@ namespace W3_TeamProject
             // 상태보기 창 -> 디자인을 추가
             LeftHiText(Cyan, "상태보기");
             Console.WriteLine();
-            ViewStatus();
-            Console.WriteLine();
+            Console.WriteLine("플레이어의 상태를 확인할 수 있습니다.");
+            ViewStatus(0, 4, 20, 9);
             StatusSelection();
         }
 
@@ -36,24 +36,12 @@ namespace W3_TeamProject
 
 
         /// <summary>
-        /// 플레이어의 상태를 확인하는 함수입니다.
+        /// 플레이어의 상태를 확인하는 함수입니다 / 시작 x좌표, 시작 y좌표, 너비, 높이
         /// </summary>
-        public void ViewStatus()
+        public void ViewStatus(int startXpos, int startYpos, int width, int height)
         {
-            Console.SetCursorPosition(5, 2);
-            Console.WriteLine($"레 벨 : {Player.Level}");
-            Console.SetCursorPosition(5, 3);
-            Console.WriteLine($"이 름 : {Player.PlayerName}");
-            Console.SetCursorPosition(5, 4);
-            ViewAtk();
-            Console.SetCursorPosition(5, 5);
-            ViewDef();
-            Console.SetCursorPosition(6, 6);
-            ViewHP();
-            Console.SetCursorPosition(6, 7);
-            ViewMP();
-            Console.SetCursorPosition(5, 8);
-            Console.WriteLine($"소지금 : {Player.Gold}");
+            MakeBorder(startXpos, startYpos, width, height);
+            StatusData(startXpos + 2, startYpos + 1);
         }
 
 
@@ -69,7 +57,10 @@ namespace W3_TeamProject
                 // if 문을 사용하여
                 // 플레이어의 입력을 받았다면
                 // 해당하는 입력에 대한 리액션을 리턴하고 break;
+
+                Console.SetCursorPosition(1, 14);
                 Console.WriteLine("0. 뒤로가기");
+                Console.SetCursorPosition(1, 15);
                 Console.Write(">> ");
                 string input = Console.ReadLine();
 
@@ -81,8 +72,11 @@ namespace W3_TeamProject
                 else
                 {
                     Console.Clear();
+                    Console.SetCursorPosition(0, 16);
                     Console.WriteLine("잘못된 입력입니다. 다시 입력해주세요");
+                    Console.SetCursorPosition(0, 0);
                     EnterScene();
+                    break;
                 }
             }
         }
@@ -131,6 +125,56 @@ namespace W3_TeamProject
             int totalMana = Player.EquipMana + Player.BaseMana;
             Console.Write("M  P : ");
             LeftHiText(Blue, $"{Player.CurrentMana} ", $"/ {totalMana}");
+        }
+
+        /// <summary>
+        /// 테두리를 만들어주는 함수 / 시작 x좌표, 시작 y좌표, 너비, 높이
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public static void MakeBorder(int startXpos, int startYpos, int width, int height)
+        {
+            Console.ForegroundColor = Cyan;
+            Console.SetCursorPosition(startXpos, startYpos);
+            Console.Write("┏");
+            Console.Write(new string('━', width));
+            Console.WriteLine("┓");
+            for (int i = startYpos + 1; i < startYpos + height; i++)
+            {
+                Console.SetCursorPosition(startXpos, i);
+                Console.Write('┃');
+                Console.SetCursorPosition(startXpos + width + 1, i);
+                Console.WriteLine('┃');
+            }
+            Console.SetCursorPosition(startXpos, startYpos + height - 1);
+            Console.Write("┗");
+            Console.Write(new string('━', width));
+            Console.WriteLine("┛");
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Status data를 보여주는 함수 / 시작 x좌표, 시작 y좌표
+        /// </summary>
+        /// <param name="startXpos"></param>
+        /// <param name="startYpos"></param>
+        public static void StatusData(int startXpos, int startYpos)
+        {
+            Console.SetCursorPosition(startXpos, startYpos);
+            Console.WriteLine($"레 벨 : {Player.Level}");
+            Console.SetCursorPosition(startXpos, startYpos + 1);
+            Console.WriteLine($"이 름 : {Player.PlayerName}");
+            Console.SetCursorPosition(startXpos, startYpos + 2);
+            ViewAtk();
+            Console.SetCursorPosition(startXpos, startYpos + 3);
+            ViewDef();
+            Console.SetCursorPosition(startXpos, startYpos + 4);
+            ViewHP();
+            Console.SetCursorPosition(startXpos, startYpos + 5);
+            ViewMP();
+            Console.SetCursorPosition(startXpos, startYpos + 6);
+            Console.WriteLine($"소지금 : {Player.Gold}");
+            Console.WriteLine();
         }
 
 
