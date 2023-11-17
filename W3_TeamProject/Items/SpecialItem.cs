576a4046ad7a7feae02a9239c6501796401e6c75
Jang-Yeongjun <150320634+Jang-Yeongjun@users.noreply.github.com>
2023-11-17 15:07:47 +0900

    Update SpecialItem.cs

--- W3_TeamProject/Items/SpecialItem.cs
+++ W3_TeamProject/Items/SpecialItem.cs
@@ -7,7 +7,7 @@ using System.Xml.Linq;
 
 namespace W3_TeamProject
 {
-	internal class WeaponItem : BaseItem
+	internal class TestItem : BaseItem
 	{
 		protected override void Init()
 		{
@@ -21,45 +21,4 @@ namespace W3_TeamProject
 			isEquip = false;
 		}
 	}
-    internal class RustySword : BaseItem
-    {
-        protected override void Init()
-        {
-            name = "녹슨 검";
-            description = "어딘가에 버려져 있어도 이상하지 않은 검입니다.";
-            cost = 400;
-            status = Status.Attack;
-            effectValue = 3;
-            itemType = ItemType.Weapon;
-            isEquip = false;
-        }
-    }
-
-    internal class SteelSword : BaseItem
-    {
-        protected override void Init()
-        {
-            name = "철검";
-            description = "잘 만들어진 철검입니다.";
-            cost = 800;
-            status = Status.Attack;
-            effectValue = 6;
-            itemType = ItemType.Weapon;
-            isEquip = false;
-        }
-    }
-
-    internal class SpartaSword : BaseItem
-    {
-        protected override void Init()
-        {
-            name = "스파르타 검";
-            description = "엄청난 기운이 느껴지는 검입니다.";
-            cost = 5000;
-            status = Status.Attack;
-            effectValue = 20;
-            itemType = ItemType.Weapon;
-            isEquip = false;
-        }
-    }
 }
