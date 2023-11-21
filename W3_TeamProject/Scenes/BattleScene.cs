d6886246cae570abdf2cf5948c2496c696a93236
ZANE <130233619+chochozane@users.noreply.github.com>
2023-11-21 12:37:37 +0900

    [EDIT] BattleScene Enemy 랜덤 출현 구현

--- W3_TeamProject/Scenes/BattleScene.cs
+++ W3_TeamProject/Scenes/BattleScene.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics.SymbolStore;
-
-namespace W3_TeamProject
+﻿namespace W3_TeamProject
 {
     internal class BattleScene : BaseScene
     {
@@ -9,19 +7,19 @@ namespace W3_TeamProject
         int userInput = 0;
         int endPoint = 0; // 0 계속, 1 플레이어 승리, 2 플레이어 패배.(BossScene 과 동일하게)
 
-        int enemyType; // ENEMY 의 타입 지정 (0 = None, 1 = ENEMY 1 , 2 = ENEMY 2 , 3 = ENEMY 3), 무조건 적이 최소 1명은 나오도록 구현하자.
-		    Controller itemController = new Controller();
-		    Controller skillController = new Controller();
-		    Random random = new Random();
+        Controller itemController = new Controller();
+        Controller skillController = new Controller();
+        Random random = new Random();
 
+        BattleUtility battleUtility = new BattleUtility();
 
-		public override void EnterScene()
+        public override void EnterScene()
         {
             Controller controller = new Controller();
             InitSkillController();
             InitItemController();
 
-			Console.Clear();
+            Console.Clear();
 
             Console.WriteLine("던전입구로 왔따.");
             Console.WriteLine();
@@ -106,34 +104,25 @@ namespace W3_TeamProject
             Console.SetCursorPosition(0, 0);
             Console.WriteLine("[1층]");
 
-			    ShowPlayer();
+            ShowPlayer();
 
-            //
-            // 적 랜덤 출현 구현 thinking..
-            //
-            int enemyCount = random.Next(1, 5); // Enemy 스폰 수( 최소 1 ~ 최대 4 마리 스폰)
-            for (int i = 0; i < enemyCount; i++)
+            // 적 랜덤 출현
+            List<BaseEnemy> enemyListForFirstStage = battleUtility.GetEnemyList();
+            
+            for (int i = 0; i < enemyListForFirstStage.Count; i++)
             {
-                int selectEnemy = random.Next(0, 3); // Enemy 종류 (슬라임, 고블린, 르탄이 중에서) 랜덤 생성
-                switch (selectEnemy)
-                {
-                    case 0: // e.g. Slime
-
-                        break;
-                    case 1: // e.g. Goblin
-
-                        break;
-                    case 2: // Rtan
-
-                        break;
-                }
+                enemyListForFirstStage[i].Show(); // 첫번째 스테이지의 적 나타나라 얍
             }
+            
+            //
+            // 다음은 컨트롤러.......
+            //
 
             // 가능하다면, x y 좌표값 받아서 아래 ShowEnemy 메서드들 단순하게 만들기 츄라이해보자 !
-            ShowEnemy1();
-            ShowEnemy2();
-            ShowEnemy3();
-            ShowEnemy4();
+            //ShowEnemy1();
+            //ShowEnemy2();
+            //ShowEnemy3();
+            //ShowEnemy4();
 
             WriteComment(" 원하시는 행동을 선택하세요.");
 
@@ -218,87 +207,87 @@ namespace W3_TeamProject
 
         private bool ShowItemList()
         {
-			ClearChoosePanel();
-			MakeItemChoicePanel();
+            ClearChoosePanel();
+            MakeItemChoicePanel();
 
-			int chooseItemCount = itemController.InputLoop();
-			if (chooseItemCount == 0) return true;
-			else
-			{
-				if (chooseItemCount == 1)
-				{
-					// 체력포션 사용
-					if (Player.HealthPotionCount == 0)
-					{
-						WriteComment("체력 포션이 없습니다.");
-						Thread.Sleep(1000);
-						return true;
-					}
-					else
-					{
-						Player.UseHealthPotion();
-						WriteComment("당신은 체력 포션을 마셨습니다!");
-						Thread.Sleep(1000);
-						return false;
-					}
-				}
-				else // 마나포션 사용
-				{
-					if (Player.ManaPotionCount == 0)
-					{
-						WriteComment("마나 포션이 없습니다.");
-						Thread.Sleep(1000);
-						return true;
-					}
-					else
-					{
-						Player.UseManaPotion();
-						WriteComment("당신은 마나 포션을 마셨습니다!");
-						Thread.Sleep(1000);
-						return false;
-					}
-				}
-			}
-		}
+            int chooseItemCount = itemController.InputLoop();
+            if (chooseItemCount == 0) return true;
+            else
+            {
+                if (chooseItemCount == 1)
+                {
+                    // 체력포션 사용
+                    if (Player.HealthPotionCount == 0)
+                    {
+                        WriteComment("체력 포션이 없습니다.");
+                        Thread.Sleep(1000);
+                        return true;
+                    }
+                    else
+                    {
+                        Player.UseHealthPotion();
+                        WriteComment("당신은 체력 포션을 마셨습니다!");
+                        Thread.Sleep(1000);
+                        return false;
+                    }
+                }
+                else // 마나포션 사용
+                {
+                    if (Player.ManaPotionCount == 0)
+                    {
+                        WriteComment("마나 포션이 없습니다.");
+                        Thread.Sleep(1000);
+                        return true;
+                    }
+                    else
+                    {
+                        Player.UseManaPotion();
+                        WriteComment("당신은 마나 포션을 마셨습니다!");
+                        Thread.Sleep(1000);
+                        return false;
+                    }
+                }
+            }
+        }
 
         private bool ShowSkillList()
         {
-			ClearChoosePanel();
-			MakeSkillChoicePanel();
-			BaseSkill? currentSkill = null;
-			SkillState skillState = SkillState.None;
+            ClearChoosePanel();
+            MakeSkillChoicePanel();
+            BaseSkill? currentSkill = null;
+            SkillState skillState = SkillState.None;
 
-			int chooseSkillCount = skillController.InputLoop();
-			if (chooseSkillCount == 0) return true;
-			else
-			{
-				chooseSkillCount--; // 돌아가기가 0번이라 인덱스와 맞추기 위한 보정
-				currentSkill = Player.UseSkill(chooseSkillCount, ref skillState);
-				if (skillState == SkillState.LackOfMana)
-				{
-					WriteComment("MP가 부족해 스킬을 사용할 수 없습니다.");
-					Thread.Sleep(1000);
-					return true;
-				}
-				else if (skillState == SkillState.IsCoolDown)
-				{
-					WriteComment("현재 쿨타임이 남아있는 스킬입니다.");
-					Thread.Sleep(1000);
-					return true;
-				}
-				else if (skillState == SkillState.OK) // 스킬 사용 성공
-				{
-					int damage = currentSkill.FixedDamage + currentSkill.VariableDamage * Player.Level;
+            int chooseSkillCount = skillController.InputLoop();
+            if (chooseSkillCount == 0) return true;
+            else
+            {
+                chooseSkillCount--; // 돌아가기가 0번이라 인덱스와 맞추기 위한 보정
+                currentSkill = Player.UseSkill(chooseSkillCount, ref skillState);
+                if (skillState == SkillState.LackOfMana)
+                {
+                    WriteComment("MP가 부족해 스킬을 사용할 수 없습니다.");
+                    Thread.Sleep(1000);
+                    return true;
+                }
+                else if (skillState == SkillState.IsCoolDown)
+                {
+                    WriteComment("현재 쿨타임이 남아있는 스킬입니다.");
+                    Thread.Sleep(1000);
+                    return true;
+                }
+                else if (skillState == SkillState.OK) // 스킬 사용 성공
+                {
+                    int damage = currentSkill.FixedDamage + currentSkill.VariableDamage * Player.Level;
 
-					// 여기에 적을 지정하거나 지정된 적을 공격하는 로직을 작성해야합니다!!!!
+                    // 여기에 적을 지정하거나 지정된 적을 공격하는 로직을 작성해야합니다!!!!
 
-					WriteComment($"{currentSkill.SkillComment} 사장님께 {damage}만큼의 데미지를 입혔습니다!");
-					Thread.Sleep(1000);
-					return false;
-				}
-				else return true;
-			}
-		}
+                    WriteComment($"{currentSkill.SkillComment} 사장님께 {damage}만큼의 데미지를 입혔습니다!");
+                    Thread.Sleep(1000);
+                    return false;
+                }
+                else return true;
+            }
+        }
 
         private void NormalDefense()
         {
@@ -368,7 +357,7 @@ namespace W3_TeamProject
 
             bool isPlayerTurn = true;
 
-			while (endPoint == 0)
+            while (endPoint == 0)
             {
                 Controller mainController = new Controller();
 
@@ -377,10 +366,10 @@ namespace W3_TeamProject
                 mainController.AddRotation(34, 26);
                 mainController.AddRotation(62, 26);
 
-				ClearChoosePanel();
-				MakeMainChoicePanel();
+                ClearChoosePanel();
+                MakeMainChoicePanel();
 
-				userInput = mainController.InputLoop();
+                userInput = mainController.InputLoop();
                 switch (userInput)
                 {
                     case 0: // 공격
@@ -522,14 +511,14 @@ namespace W3_TeamProject
             Console.SetCursorPosition(79, 28);
             Console.Write("돌아가기");
 
-			// 이하는 상황에 맞는 스킬 목록 작성
-			for (int i = 0; i < Player.playerSkillList.SkillCount; i++)
-			{
-				Console.SetCursorPosition(35, 22 + i);
-				BaseSkill tempSkill = Player.GetSkill(i);
-				Console.Write($"{tempSkill.SkillName} | Cost : {tempSkill.Cost} | Cooldown : {tempSkill.CurrentCooldown}");
-			}
-		}
+            // 이하는 상황에 맞는 스킬 목록 작성
+            for (int i = 0; i < Player.playerSkillList.SkillCount; i++)
+            {
+                Console.SetCursorPosition(35, 22 + i);
+                BaseSkill tempSkill = Player.GetSkill(i);
+                Console.Write($"{tempSkill.SkillName} | Cost : {tempSkill.Cost} | Cooldown : {tempSkill.CurrentCooldown}");
+            }
+        }
 
         public void ClearChoosePanel()
         {
@@ -630,98 +619,98 @@ namespace W3_TeamProject
             Console.WriteLine("---------------");
         }
 
-		private void InitSkillController()
-		{
-			// 기존 데이터 초기화
-			skillController.RemoveAll();
+        private void InitSkillController()
+        {
+            // 기존 데이터 초기화
+            skillController.RemoveAll();
 
-			skillController.AddRotation(77, 28); // 돌아가기 할당
-			int count = Player.playerSkillList.SkillCount;
+            skillController.AddRotation(77, 28); // 돌아가기 할당
+            int count = Player.playerSkillList.SkillCount;
 
-			// 현재까지 스킬은 최대 6개 상정
-			if (count == null)
-				count = 0;
-			else if (count > 6)
-				count = 6;
+            // 현재까지 스킬은 최대 6개 상정
+            if (count == null)
+                count = 0;
+            else if (count > 6)
+                count = 6;
 
-			for (int i = 0; i < count; i++)
-			{
-				// 해당 위치에 커서만 생성하도록
-				skillController.AddRotation(33, 22 + i);
-			}
-		}
-		private void InitItemController()
-		{
-			// 기존 데이터 초기화
-			itemController.RemoveAll();
+            for (int i = 0; i < count; i++)
+            {
+                // 해당 위치에 커서만 생성하도록
+                skillController.AddRotation(33, 22 + i);
+            }
+        }
+        private void InitItemController()
+        {
+            // 기존 데이터 초기화
+            itemController.RemoveAll();
 
-			itemController.AddRotation(77, 28); // 돌아가기 할당
-			itemController.AddRotation(37, 26); // 체력 할당
-			itemController.AddRotation(65, 26); // 마나 할당
-		}
+            itemController.AddRotation(77, 28); // 돌아가기 할당
+            itemController.AddRotation(37, 26); // 체력 할당
+            itemController.AddRotation(65, 26); // 마나 할당
+        }
 
-		private void MakeItemChoicePanel()
-		{
-			Console.SetCursorPosition(79, 28);
-			Console.Write("돌아가기");
-			// 체력포션, 아이템포션
-			Console.SetCursorPosition(36, 22);
-			Console.Write("┌────────────────────┐");
-			Console.SetCursorPosition(36, 23);
-			Console.Write('│');
-			Console.SetCursorPosition(36 + 21, 23);
-			Console.Write('│');
-			Console.SetCursorPosition(36, 24);
-			Console.Write('│');
-			Console.SetCursorPosition(36 + 21, 24);
-			Console.Write('│');
-			Console.SetCursorPosition(36, 25);
-			Console.Write("└────────────────────┘");
+        private void MakeItemChoicePanel()
+        {
+            Console.SetCursorPosition(79, 28);
+            Console.Write("돌아가기");
+            // 체력포션, 아이템포션
+            Console.SetCursorPosition(36, 22);
+            Console.Write("┌────────────────────┐");
+            Console.SetCursorPosition(36, 23);
+            Console.Write('│');
+            Console.SetCursorPosition(36 + 21, 23);
+            Console.Write('│');
+            Console.SetCursorPosition(36, 24);
+            Console.Write('│');
+            Console.SetCursorPosition(36 + 21, 24);
+            Console.Write('│');
+            Console.SetCursorPosition(36, 25);
+            Console.Write("└────────────────────┘");
 
-			Console.SetCursorPosition(64, 22);
-			Console.Write("┌────────────────────┐");
-			Console.SetCursorPosition(64, 23);
-			Console.Write('│');
-			Console.SetCursorPosition(64 + 21, 23);
-			Console.Write('│');
-			Console.SetCursorPosition(64, 24);
-			Console.Write('│');
-			Console.SetCursorPosition(64 + 21, 24);
-			Console.Write('│');
-			Console.SetCursorPosition(64, 25);
-			Console.Write("└────────────────────┘");
+            Console.SetCursorPosition(64, 22);
+            Console.Write("┌────────────────────┐");
+            Console.SetCursorPosition(64, 23);
+            Console.Write('│');
+            Console.SetCursorPosition(64 + 21, 23);
+            Console.Write('│');
+            Console.SetCursorPosition(64, 24);
+            Console.Write('│');
+            Console.SetCursorPosition(64 + 21, 24);
+            Console.Write('│');
+            Console.SetCursorPosition(64, 25);
+            Console.Write("└────────────────────┘");
 
-			Console.SetCursorPosition(42, 23);
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.Write("체력");
-			Console.ResetColor();
-			Console.Write(" 포션");
+            Console.SetCursorPosition(42, 23);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("체력");
+            Console.ResetColor();
+            Console.Write(" 포션");
 
-			Console.SetCursorPosition(40, 24);
-			Console.Write("체력 ");
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.Write("50% ");
-			Console.ResetColor();
-			Console.Write("회복");
+            Console.SetCursorPosition(40, 24);
+            Console.Write("체력 ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("50% ");
+            Console.ResetColor();
+            Console.Write("회복");
 
-			Console.SetCursorPosition(70, 23);
-			Console.ForegroundColor = ConsoleColor.Blue;
-			Console.Write("마나");
-			Console.ResetColor();
-			Console.Write(" 포션");
+            Console.SetCursorPosition(70, 23);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("마나");
+            Console.ResetColor();
+            Console.Write(" 포션");
 
-			Console.SetCursorPosition(68, 24);
-			Console.Write("마나 ");
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.Write("50% ");
-			Console.ResetColor();
-			Console.Write("회복");
+            Console.SetCursorPosition(68, 24);
+            Console.Write("마나 ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("50% ");
+            Console.ResetColor();
+            Console.Write("회복");
 
-			Console.SetCursorPosition(39, 26);
-			Console.Write($"남은 포션 : {Player.HealthPotionCount}개");
+            Console.SetCursorPosition(39, 26);
+            Console.Write($"남은 포션 : {Player.HealthPotionCount}개");
 
-			Console.SetCursorPosition(67, 26);
-			Console.Write($"남은 포션 : {Player.ManaPotionCount}개");
-		}
-	}
+            Console.SetCursorPosition(67, 26);
+            Console.Write($"남은 포션 : {Player.ManaPotionCount}개");
+        }
+    }
 }
