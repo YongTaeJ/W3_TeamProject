fc81783452be94794c762c9b1bc5a4fde71ea145
YongTaeJ <g_o_robot@naver.com>
2023-11-21 11:13:29 +0900

    BattleUtility 작성, 기본적인 Enemy 작성

---
+++ W3_TeamProject/Enemies/Goblin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W3_TeamProject
+{
+	internal class Goblin : BaseEnemy
+	{
+		public Goblin(int level) : base(level)
+		{
+
+		}
+
+		protected override void Init(int level)
+		{
+			name = "고블린씨";
+
+			this.level = level;
+
+			health = 30 + 7 * level;
+			attack = 5 + 1 * level;
+			currentHealth = health;
+		}
+	}
+}
